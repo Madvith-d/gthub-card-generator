1b4f9f921ddbc5072ff0e6dc56df8c3595dec741
Madvith-d <madroux930@gmail.com>
2025-07-06 08:45:17 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+  const name = "Madvith"
+  return(
+    <>
+     <Card  name = {name}   a="hello"/>
+    
+    </>
+  )
+  
+}
+
+export default App
---
+++ src/components/Card.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+
+const Card= (props)=>{
+
+    console.log(props)
+
+    return(
+        <>
+            <h1>Hello</h1>        
+        
+        </>
+    )
+}
+
+export default  Card
